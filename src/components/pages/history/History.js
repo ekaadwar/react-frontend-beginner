ac3972a139e8d53d15596b364aee6a6a2fe4cb79
Eka Fajhari Adwar <eka.adwar@gmail.com>
2021-06-09 12:04:37 +0700

    Create content for History page

--- src/components/pages/history/History.js
+++ src/components/pages/history/History.js
@@ -1,9 +1,214 @@
 import React from "react"
 
 class History extends React.Component{
-    render(){
-        return <h2>History</h2>;
-    }
+  render(){
+    return(
+      <section class="history pt-20 pb-40 bg-gray-200">
+        <div class="container mx-auto">
+          <h2 class="pt-40 pb-3 text-5xl font-bold text-white text-shadow text-center">Let’s see what you have bought!</h2>
+          <p class="text-xl text-white text-shadow text-center">Select item to delete</p>
+          <p class="text-xl text-white text-shadow text-right"><a href="#">Delete</a></p>
+          <div class="grid grid-cols-3 gap-x-3 gap-y-5">
+            <div class="flex justify-between p-5 bg-white rounded-2xl">
+              <div class="h-20 w-20 overflow-hidden rounded-full bg-gray-300"><img class="w-full" src="img/produk/vergieTomato.png" alt="Vergie Tomato Mixed" /></div>
+              <div>
+                <h4 class="text-2xl font-bold">Vergie tomato mix</h4>
+                <div class="flex justify-between items-end text-yellow-900">
+                  <div>
+                    <p>IDR 34.0000</p>
+                    <p>delivered</p>
+                  </div>
+                  <input type="checkbox" name="vergie" />
+                </div>
+              </div>
+            </div>
+            <div class="flex justify-between p-5 bg-white rounded-2xl">
+              <div class="h-20 w-20 overflow-hidden rounded-full bg-gray-300"><img class="w-full" src="img/produk/vergieTomato.png" alt="Vergie Tomato Mixed" /></div>
+              <div>
+                <h4 class="text-2xl font-bold">Vergie tomato mix</h4>
+                <div class="flex justify-between items-end text-yellow-900">
+                  <div>
+                    <p>IDR 34.0000</p>
+                    <p>delivered</p>
+                  </div>
+                  <input type="checkbox" name="vergie" />
+                </div>
+              </div>
+            </div>
+            <div class="flex justify-between p-5 bg-white rounded-2xl">
+              <div class="h-20 w-20 overflow-hidden rounded-full bg-gray-300"><img class="w-full" src="img/produk/vergieTomato.png" alt="Vergie Tomato Mixed" /></div>
+              <div>
+                <h4 class="text-2xl font-bold">Vergie tomato mix</h4>
+                <div class="flex justify-between items-end text-yellow-900">
+                  <div>
+                    <p>IDR 34.0000</p>
+                    <p>delivered</p>
+                  </div>
+                  <input type="checkbox" name="vergie" />
+                </div>
+              </div>
+            </div>
+            <div class="flex justify-between p-5 bg-white rounded-2xl">
+              <div class="h-20 w-20 overflow-hidden rounded-full bg-gray-300"><img class="w-full" src="img/produk/vergieTomato.png" alt="Vergie Tomato Mixed" /></div>
+              <div>
+                <h4 class="text-2xl font-bold">Vergie tomato mix</h4>
+                <div class="flex justify-between items-end text-yellow-900">
+                  <div>
+                    <p>IDR 34.0000</p>
+                    <p>delivered</p>
+                  </div>
+                  <input type="checkbox" name="vergie" />
+                </div>
+              </div>
+            </div>
+            <div class="flex justify-between p-5 bg-white rounded-2xl">
+              <div class="h-20 w-20 overflow-hidden rounded-full bg-gray-300"><img class="w-full" src="img/produk/vergieTomato.png" alt="Vergie Tomato Mixed" /></div>
+              <div>
+                <h4 class="text-2xl font-bold">Vergie tomato mix</h4>
+                <div class="flex justify-between items-end text-yellow-900">
+                  <div>
+                    <p>IDR 34.0000</p>
+                    <p>delivered</p>
+                  </div>
+                  <input type="checkbox" name="vergie" />
+                </div>
+              </div>
+            </div>
+            <div class="flex justify-between p-5 bg-white rounded-2xl">
+              <div class="h-20 w-20 overflow-hidden rounded-full bg-gray-300"><img class="w-full" src="img/produk/vergieTomato.png" alt="Vergie Tomato Mixed" /></div>
+              <div>
+                <h4 class="text-2xl font-bold">Vergie tomato mix</h4>
+                <div class="flex justify-between items-end text-yellow-900">
+                  <div>
+                    <p>IDR 34.0000</p>
+                    <p>delivered</p>
+                  </div>
+                  <input type="checkbox" name="vergie" />
+                </div>
+              </div>
+            </div>
+            <div class="flex justify-between p-5 bg-white rounded-2xl">
+              <div class="h-20 w-20 overflow-hidden rounded-full bg-gray-300"><img class="w-full" src="img/produk/vergieTomato.png" alt="Vergie Tomato Mixed" /></div>
+              <div>
+                <h4 class="text-2xl font-bold">Vergie tomato mix</h4>
+                <div class="flex justify-between items-end text-yellow-900">
+                  <div>
+                    <p>IDR 34.0000</p>
+                    <p>delivered</p>
+                  </div>
+                  <input type="checkbox" name="vergie" />
+                </div>
+              </div>
+            </div>
+            <div class="flex justify-between p-5 bg-white rounded-2xl">
+              <div class="h-20 w-20 overflow-hidden rounded-full bg-gray-300"><img class="w-full" src="img/produk/vergieTomato.png" alt="Vergie Tomato Mixed" /></div>
+              <div>
+                <h4 class="text-2xl font-bold">Vergie tomato mix</h4>
+                <div class="flex justify-between items-end text-yellow-900">
+                  <div>
+                    <p>IDR 34.0000</p>
+                    <p>delivered</p>
+                  </div>
+                  <input type="checkbox" name="vergie" />
+                </div>
+              </div>
+            </div>
+            <div class="flex justify-between p-5 bg-white rounded-2xl">
+              <div class="h-20 w-20 overflow-hidden rounded-full bg-gray-300"><img class="w-full" src="img/produk/vergieTomato.png" alt="Vergie Tomato Mixed" /></div>
+              <div>
+                <h4 class="text-2xl font-bold">Vergie tomato mix</h4>
+                <div class="flex justify-between items-end text-yellow-900">
+                  <div>
+                    <p>IDR 34.0000</p>
+                    <p>delivered</p>
+                  </div>
+                  <input type="checkbox" name="vergie" />
+                </div>
+              </div>
+            </div>
+            <div class="flex justify-between p-5 bg-white rounded-2xl">
+              <div class="h-20 w-20 overflow-hidden rounded-full bg-gray-300"><img class="w-full" src="img/produk/vergieTomato.png" alt="Vergie Tomato Mixed" /></div>
+              <div>
+                <h4 class="text-2xl font-bold">Vergie tomato mix</h4>
+                <div class="flex justify-between items-end text-yellow-900">
+                  <div>
+                    <p>IDR 34.0000</p>
+                    <p>delivered</p>
+                  </div>
+                  <input type="checkbox" name="vergie" />
+                </div>
+              </div>
+            </div>
+            <div class="flex justify-between p-5 bg-white rounded-2xl">
+              <div class="h-20 w-20 overflow-hidden rounded-full bg-gray-300"><img class="w-full" src="img/produk/vergieTomato.png" alt="Vergie Tomato Mixed" /></div>
+              <div>
+                <h4 class="text-2xl font-bold">Vergie tomato mix</h4>
+                <div class="flex justify-between items-end text-yellow-900">
+                  <div>
+                    <p>IDR 34.0000</p>
+                    <p>delivered</p>
+                  </div>
+                  <input type="checkbox" name="vergie" />
+                </div>
+              </div>
+            </div>
+            <div class="flex justify-between p-5 bg-white rounded-2xl">
+              <div class="h-20 w-20 overflow-hidden rounded-full bg-gray-300"><img class="w-full" src="img/produk/vergieTomato.png" alt="Vergie Tomato Mixed" /></div>
+              <div>
+                <h4 class="text-2xl font-bold">Vergie tomato mix</h4>
+                <div class="flex justify-between items-end text-yellow-900">
+                  <div>
+                    <p>IDR 34.0000</p>
+                    <p>delivered</p>
+                  </div>
+                  <input type="checkbox" name="vergie" />
+                </div>
+              </div>
+            </div>
+            <div class="flex justify-between p-5 bg-white rounded-2xl">
+              <div class="h-20 w-20 overflow-hidden rounded-full bg-gray-300"><img class="w-full" src="img/produk/vergieTomato.png" alt="Vergie Tomato Mixed" /></div>
+              <div>
+                <h4 class="text-2xl font-bold">Vergie tomato mix</h4>
+                <div class="flex justify-between items-end text-yellow-900">
+                  <div>
+                    <p>IDR 34.0000</p>
+                    <p>delivered</p>
+                  </div>
+                  <input type="checkbox" name="vergie" />
+                </div>
+              </div>
+            </div>
+            <div class="flex justify-between p-5 bg-white rounded-2xl">
+              <div class="h-20 w-20 overflow-hidden rounded-full bg-gray-300"><img class="w-full" src="img/produk/vergieTomato.png" alt="Vergie Tomato Mixed" /></div>
+              <div>
+                <h4 class="text-2xl font-bold">Vergie tomato mix</h4>
+                <div class="flex justify-between items-end text-yellow-900">
+                  <div>
+                    <p>IDR 34.0000</p>
+                    <p>delivered</p>
+                  </div>
+                  <input type="checkbox" name="vergie" />
+                </div>
+              </div>
+            </div>
+            <div class="flex justify-between p-5 bg-white rounded-2xl">
+              <div class="h-20 w-20 overflow-hidden rounded-full bg-gray-300"><img class="w-full" src="img/produk/vergieTomato.png" alt="Vergie Tomato Mixed" /></div>
+              <div>
+                <h4 class="text-2xl font-bold">Vergie tomato mix</h4>
+                <div class="flex justify-between items-end text-yellow-900">
+                  <div>
+                    <p>IDR 34.0000</p>
+                    <p>delivered</p>
+                  </div>
+                  <input type="checkbox" name="vergie" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default History
