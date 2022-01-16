bb0de69046a980ca4b66e1c5224a95e7ce9877e8
Eka Fajhari Adwar <eka.adwar@gmail.com>
2022-01-17 02:15:07 +0700

    responsif cart page

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,5 +3,6 @@ import React from "react";
 import hazelnutLatte from "./hazelnutLatte.png";
 import pinkyPromise from "./pinkyPromise.png";
 import chikenWing from "./chikenWing.png";
+import chickenFireWing from "./chickenFireWing.png";
 
-export { hazelnutLatte, pinkyPromise, chikenWing };
+export { hazelnutLatte, pinkyPromise, chikenWing, chickenFireWing };
