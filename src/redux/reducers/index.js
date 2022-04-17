14de49fedd1a07445a6dc75100dad09337ff7612
Eka Fajhari Adwar <eka.adwar@gmail.com>
2022-04-17 17:28:30 +0700

    Install redux-persist; kofigurasi redux-persist di redux store, kofigurasi redux-persist di redux reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,12 +1,19 @@
+import storage from "redux-persist/lib/storage";
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
 import carts from "./carts";
 import auth from "./auth";
 import items from "./items";
 import products from "./products";
 
+const persistAuth = {
+  storage,
+  key: "auth",
+};
+
 const reducer = combineReducers({
   carts,
-  auth,
+  auth: persistReducer(persistAuth, auth),
   items,
   products,
 });
